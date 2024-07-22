8f776595e26f33b5e0af6f255898f51cc95ab86d
Aleemendoza <alecolomendoza@gmail.com>
2024-07-22 20:21:26 -0300

    fix: update paquetes todos

--- pages/paquetes-todos.js
+++ pages/paquetes-todos.js
@@ -9,15 +9,14 @@ export default function Servicios() {
     { name: "Inicio", href: "/" },
     { name: "Servicios", href: "/" },
     { name: "Contacto", href: "/" },
-    //  { name: "Paquetes", href: "/paquetes-todos" },
     // Añade más enlaces según sea necesario
   ];
 
   return (
     <div>
       <Navbar links={links} />
-      <Container className="grid h-[90%] justify-center items-center ">
-        <header className="mt-20 mb-12 text-center px-8 ">
+      <Container className="grid h-[90%] justify-center items-center">
+        <header className="mt-20 mb-12 text-center px-8">
           <h2 className="text-[35px] font-bold leading-snug tracking-tight text-[#4888CD] lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
             Paquetes de Diseño Web
           </h2>
@@ -29,7 +28,7 @@ export default function Servicios() {
               <p className="text-gray-700 dark:text-gray-300 text-lg">
                 Contamos con 4 Paquetes de Diseño Web que incluyen todo lo que
                 necesitas para tener un sitio web para promocionar tu negocio,
-                incrementar tus ventas, posicionarte en buscadores
+                incrementar tus ventas, y posicionarte en buscadores.
               </p>
             </div>
             <div className="bg-gray-200 dark:bg-gray-700 rounded-lg">
@@ -38,7 +37,7 @@ export default function Servicios() {
                 width={500}
                 height={500}
                 alt="paquetes"
-                className=" w-full rounded-lg object-cover"
+                className="w-full rounded-lg object-cover"
               />
             </div>
           </div>
@@ -52,23 +51,22 @@ export default function Servicios() {
               <p>Descripción breve del Paquete 1</p>
             </div>
           </Link>
-          {/* PAQUETE 2  */}
+          {/* PAQUETE 2 */}
           <Link href={"/paquete2"}>
             <div className="p-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg text-center">
               <h4 className="text-xl font-semibold mb-4">Paquete 2</h4>
               <p>Descripción breve del Paquete 2</p>
             </div>
           </Link>
-          {/* PAQUETE 3  */}
+          {/* PAQUETE 3 */}
           <Link href={"/paquete3"}>
             <div className="p-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg text-center">
               <h4 className="text-xl font-semibold mb-4">Paquete 3</h4>
               <p>Descripción breve del Paquete 3</p>
             </div>
           </Link>
-
-          {/* PAQUETE 4  */}
-          <Link href={"/paquete3"}>
+          {/* PAQUETE 4 */}
+          <Link href={"/paquete4"}>
             <div className="p-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg text-center">
               <h4 className="text-xl font-semibold mb-4">Paquete 4</h4>
               <p>Descripción breve del Paquete 4</p>
@@ -84,21 +82,42 @@ export default function Servicios() {
           <span className="text-[#4888CD]">Incluyen:</span>
         </h3>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {[...Array(3)].map((_, i) => (
-            <div
-              key={i}
-              className="p-6 bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg"
-            >
-              <h4 className="text-xl font-semibold mb-4">TITULO {i + 1}</h4>
-              <p className="text-gray-700 dark:text-gray-300">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                semper consequat libero a consequat. Cras ac volutpat ex, nec
-                posuere metus. Praesent porttitor iaculis condimentum. Praesent
-                malesuada mi nunc, non auctor ex scelerisque id. Sed sed
-                pellentesque lorem, eu blandit risus.
-              </p>
-            </div>
-          ))}
+          <div className="p-6 bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg">
+            <h4 className="text-xl font-semibold mb-4">Asesoría Gratuita</h4>
+            <p className="text-gray-700 dark:text-gray-300">
+              Asesoría gratuita durante el proceso para ayudarte a despejar dudas y brindarte soluciones.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg">
+            <h4 className="text-xl font-semibold mb-4">Equipo Experto</h4>
+            <p className="text-gray-700 dark:text-gray-300">
+              Un equipo exclusivo y experto en diseño y desarrollo web profesional para ayudarte con cada paso.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg">
+            <h4 className="text-xl font-semibold mb-4">Diseño Personalizado</h4>
+            <p className="text-gray-700 dark:text-gray-300">
+              Diseños de páginas web personalizados y optimizados para dispositivos móviles.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg">
+            <h4 className="text-xl font-semibold mb-4">Optimización</h4>
+            <p className="text-gray-700 dark:text-gray-300">
+              Sitio web/tienda en línea optimizada para carga rápida y posicionamiento en buscadores.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg">
+            <h4 className="text-xl font-semibold mb-4">Hosting</h4>
+            <p className="text-gray-700 dark:text-gray-300">
+              Hasta 20GB de hosting para alojar tu aplicacion web y una copia de seguridad por mes.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg">
+            <h4 className="text-xl font-semibold mb-4">Seguridad</h4>
+            <p className="text-gray-700 dark:text-gray-300">
+              Desarrollo con altos estándares de seguridad.
+            </p>
+          </div>
         </div>
       </Container>
 
